ff54af4331ae06b9f976b5007a499662d53ca95a
Maureen <oki.maureen.eloho@gmail.com>
2020-03-11 12:40:46 +0100

    restructure app.ts to use class and typescript

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,37 @@
+import dotenv from "dotenv"
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-const port = process.env.PORT || 8000;
 
-app.get('*', (req, res) => res.status(200).send({
-   message: 'Welcome to ticket-manager API.'
-}));
-app.listen(port, () => {
-   // tslint:disable-next-line:no-console
-   console.log(`Server is running on PORT ${port}`);
-});
-export default app;
+// initialize configuration
+dotenv.config();
+
+class App{
+   public app: express.Application = express();
+   constructor(){
+      this.app;
+   }
+   private config(): void{
+      this.app.use(bodyParser.json())
+      this.app.use(bodyParser.urlencoded({extended:false}))
+   }
+
+}
+export default new App().app
+
+
+
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// const port = process.env.PORT || 8000;
+
+// app.get('*', (req, res) => res.status(200).send({
+//    message: 'Welcome to ticket-manager API.'
+// }));
+// app.listen(port, () => {
+//    // tslint:disable-next-line:no-console
+//    console.log(`Server is running on PORT ${port}`);
+// });
+// export default app;
 
